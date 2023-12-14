2165d63c79ab8a2d0f791cf83777d2ebaac70d6f
Avi Kumar <149712920+frogsjumps@users.noreply.github.com>
2023-12-13 17:48:03 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+
+int userAnswer;
+int userAnswer2;
+int userAnswer3;
+
+Console.WriteLine("Welcome to the math game! Solve these problems: ");
+Console.WriteLine("");
+
+Console.Write("15 + 2 * 2 = ");
+userAnswer = Convert.ToInt32( Console.ReadLine() );
+
+if(userAnswer == 19){
+    Console.WriteLine("Correct!");
+} else {
+    Console.WriteLine("Wrong!");
+}
+
+Console.Write("10 * 2 / 4 = ");
+userAnswer2 = Convert.ToInt32(Console.ReadLine() );
+
+if (userAnswer2 == 5){
+    Console.WriteLine("Correct!");
+} else {
+    Console.WriteLine("Wrong!");
+}
+
+Console.Write("(4 + 3 + 2 + 1) / 2 = ");
+userAnswer3 = Convert.ToInt32(Console.ReadLine());
+
+if (userAnswer3 == 5){
+    Console.WriteLine("Correct!");
+} else {
+    Console.WriteLine("Wrong!");
+}
+
+Console.ReadKey();
+
+
+
